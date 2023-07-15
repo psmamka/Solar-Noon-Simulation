bd1c8165b88eae94965be18db45838dd4a8c4772
P S Mamkani <psmamkani@protonmail.com>
2023-07-15 16:35:46 -0700

    calculate the planetary rotation period

--- Engine/field_2d.py
+++ Engine/field_2d.py
@@ -33,6 +33,7 @@ class Field2D:
         self.vx_ar[0], self.vy_ar[0] = (self.v_0[0], self.v_0[1])
 
         self.orbit_calculated = False
+        self.orbital_period = 0
 
     def calc_next_point(self, 
                   x, y, 
@@ -118,11 +119,28 @@ class Field2D:
             if verbose and dist < 10 * eps: 
                 print("Orbital point: ", dist, self.time_arr[idx], idx)
             if dist < eps:
+                self.orbital_period = self.time_arr[idx]
                 return (self.time_arr[idx], idx)
-            
-        return (0.0, 0)
-            
         
+        print("could not calculate the orbital period.")
+        exit()
+    
+    # the length of the astronomical/sidereal day based on the orbital period and num days/year
+    # astronomical day is slightly shorter than the solar/synodic day if the two rotational
+    # motions are in the same directions. e.g. for earch.
+    def calculate_rotational_period(self, days_per_year=365.256, rotations_aligned=True):
+        if self.orbital_period == 0: self.calculate_orbital_period()
+
+        # calculate number of rotations:
+        rotations_per_year = \
+            days_per_year + 1 if rotations_aligned else days_per_year - 1
+
+        self.rotational_period = self.orbital_period / rotations_per_year
+
+        return self.rotational_period
+
+
+
 
 
 
